c9544fa86c1d60a64906e28b2258c9eb1c094550
Guilherme Faria <guilherme_fgl@hotmail.com>
2018-10-08 20:39:30 -0300

    spring starter

---
+++ src/main/java/com/guilherme/springmicroservicecloud/SpringMicroserviceCloudApplication.java
@@ -0,0 +1,12 @@
+package com.guilherme.springmicroservicecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceCloudApplication.class, args);
+	}
+}
